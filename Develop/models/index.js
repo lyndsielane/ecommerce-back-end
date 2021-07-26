676ecb3d4fea6355ef9f0d8ad8153086bce9fd9b
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-26 16:13:26 -0400

    datatypes started

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,27 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { DataTypes } = require('sequelize/types');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'productTag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'productTag'
+});
 
 module.exports = {
   Product,
