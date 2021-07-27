15bccec3c0de0c2decbc05a815780d623194aac1
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-27 19:13:33 -0400

    fixed routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -37,9 +37,7 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new tag
   try {
-    const new_tag = await Tag.create({
-      tag_name: req.body.tag_name,
-    });
+    const new_tag = await Tag.create(req.body);
     res.status(200).json(new_tag);
   } catch (err) {
     res.status(400).json(err);
@@ -48,16 +46,11 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
-  await Tag.update(
-    {
-      tag_name: req.body.tag_name,
+  await Tag.update(req.body, {
+    where: {
+      id: req.params.id,
     },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
+  })
     .then((updatedTag) => {
       res.json(updatedTag);
     })
